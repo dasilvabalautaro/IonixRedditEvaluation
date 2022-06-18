112e21a1aec5cae7a00c604a1d5b7b14126a3df5
Arturo Silva <david.arturo.silva@gmail.com>
2022-06-18 11:05:10 -0400

    Initial commit for evaluation. Libraries configuration.

---
+++ app/src/main/java/com/globalhiddenodds/ionixredditevaluation/IonixRedditApplication.kt
@@ -0,0 +1,7 @@
+package com.globalhiddenodds.ionixredditevaluation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class IonixRedditApplication: Application()
